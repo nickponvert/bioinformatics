194ba5d42193ce8ff9067d0a25e6fe9220f46f17
Nick Ponvert <nickponvert@gmail.com>
2014-02-05 23:03:55 -0800

    Remove old complexity test script

--- intro.py
+++ intro.py
@@ -8,5 +8,13 @@ for i in range(10):
 
 def hello(hello):
 	See!! That worked
+class NewClass(none):
+    """docstring"""
+    def __init__(self, String):
+        super(NewClass, self).__init__()
+        self.String = String
 
-
+for i in range(19):
+    
+if condition:
+    
